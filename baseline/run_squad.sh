f775ec4edd6fb7ec4d9fa465b24a5b1035c8a27d
ChrisWoo0443 <chefwoodini@gmail.com>
2025-08-22 05:42:55 -0700

    updated script

--- baseline/run_squad.sh
+++ baseline/run_squad.sh
@@ -2,10 +2,11 @@
 # export DATA_DIR=./data/squadv2
 # export MODEL_DIR=./models
 
+# change batch size depending on gpu 
 python ../transformers/examples/legacy/question-answering/run_squad_baseline.py  \
     --model_type distilbert   \
     --model_name_or_path distilbert-base-uncased  \
-    --output_dir ./outputs/finetuning-baseline \
+    --output_dir ./outputs/distilbert-finetuning-baseline \
     --data_dir data/squadv2   \
     --overwrite_output_dir \
     --overwrite_cache \
@@ -15,8 +16,8 @@ python ../transformers/examples/legacy/question-answering/run_squad_baseline.py 
     --do_lower_case  \
     --do_eval   \
     --predict_file dev-v2.0.json   \
-    --per_gpu_train_batch_size 16   \
-    --per_gpu_eval_batch_size 16   \
+    --per_gpu_train_batch_size 128   \
+    --per_gpu_eval_batch_size 128   \
     --learning_rate 2e-5   \
     --num_train_epochs 3   \
     --weight_decay 0.01   \
